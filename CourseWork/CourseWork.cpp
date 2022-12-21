05be1c6ce2428b008a121b539c2afa5e20204ad2
Leon <33493030+leonnikonov@users.noreply.github.com>
2022-12-21 17:34:00 +0400

    Add files via upload

---
+++ CourseWork/CourseWork.cpp
@@ -0,0 +1,14 @@
+﻿// CourseWork.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include "words.h"
+
+using namespace std;
+int main()
+{
+    string* words = readWords(7);
+    cout << "Hello World!\n" << words[0];
+}
+
