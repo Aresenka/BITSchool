b6261fecb0984772d336666c07883c79db12f2e0
GeoBearD <geobeardstrateg1@gmail.com>
2023-01-10 07:54:40 -0800

    1.10.2023

--- CourseWork/Wordly/Solutions/224232 group/Dimitri Avetisyan/src/CourseWork.cpp
+++ CourseWork/Wordly/Solutions/224232 group/Dimitri Avetisyan/src/CourseWork.cpp
@@ -9,47 +9,38 @@
 #include <stdio.h> 
 using namespace std;
 
-void startGame(char y, char n) {
-    char a;
-    cin >> a;
-    if (a == 'y') {
 
-    }
-}
 
-int main()
-
+void game()
 {
     cout << "\t\t\t\t\t  \033[1;34m  Lets play the Game GALLOWS!\033[0m" << endl;
     cout << " \t\t\t\t\t \033[1;34mChoose, how much char in word(4-7): \033[0m" << endl;
     int nChar, i;
     string randomWord;
-    
-
 
     while (true) {
         cin >> nChar;
         if (nChar < 4 || nChar > 7) {
-            cout << "\033[1;31mFail, choose from 4 to 7\033[0m" << endl;
+            cout << "\t\t\t\t\t \033[1;31mFail, choose from 4 to 7\033[0m" << endl;
+            
         }
         else {
             break;
         }
-        }
+    }
     cout << "\t\t\t\t\t     \033[1;30m    You have 10 attempts\033[0m " << endl;
     if (nChar >= 4 || nChar <= 7) {
         srand(time(0));
         int length = countLinesInFile(openFileForWordsWithLenght(nChar));
         i = rand() % length;
         string* words = readWords(nChar);
-        //cout << words[i] << endl;
+        cout << words[i] << endl;// УДАЛИТЬ !
         randomWord = words[i];
-
     }
 
     for (int i = 0; i < nChar; i++)
     {
-        cout << "_ ";
+        cout << " _ ";
     }
     cout << endl;
     string enterWord;
@@ -58,7 +49,7 @@ int main()
         while (true) {
             cin >> enterWord;
             if (enterWord.length() > nChar) {
-                cout << "\033[1;31mWord is too long\033[0m" << endl;
+                cout << " \033[1;31mWord is too long\033[0m" << endl;
             }
 
             if (enterWord.length() < nChar) {
@@ -72,7 +63,8 @@ int main()
             for (int j = 0; j < randomWord.length() - 1; j++)
             {
                 if (i != j && randomWord[i] == enterWord[j]) {
-                    cout << enterWord[j] << "\033[1;31m in the word on another place!\033[0m" << endl;
+                    cout <<"\033[1;31m" << enterWord[j] << "\033[0m"<<"\033[1;30m in the word on another place!\033[0m" << endl;
+                    break;  //возможно удалить!
                 }
 
             }
@@ -80,11 +72,11 @@ int main()
         for (int i = 0; i < randomWord.length() - 1; i++)
         {
             if (randomWord[i] == enterWord[i]) {
-                cout << enterWord[i] ;
+                cout << "\033[1;32m" << enterWord[i] << "\033[0m";
                 rightChars++;
             }
             else {
-                cout << "_ ";
+                cout << " _ ";
             }
         }
         if (rightChars == randomWord.length() - 1) {
@@ -95,11 +87,37 @@ int main()
             rightChars = 0;
         }
 
-        cout << endl << "\t\t\t\t\t    \033[1;30m      Attempts -  \033[0m" << chance << endl;
+        cout << endl << "\t\t\t\t\t    \033[1;30m      Attempts - " << chance << "\033[0m" << endl;
         if (chance == 0) {
             cout << "\t\t\t\t\t     \033[1;31m      GAME OVER!\033[0m" << endl;
-            cout << "\t\t\t\t\t\t \033[1;31m Word was - \033[0m"<<randomWord << endl;
+            cout << "\t\t\t\t\t\t \033[1;31m Word was - \033[0m" << "\033[1; 30m"<< randomWord <<"\033[0m" << endl;
         }
     }
+}
+
+void startGame() {
+    
+    char a;
+    while (true) {
+        cout << "\t\t\t\t\t  \033[1;34m  Do u want to play again?\033[0m" << endl;
+        cout << "\t\t\t\t\t  \033[1;34m     Enter Y/y if u want.\033[0m" << endl;
+        cin >> a;
+        if (a == 'y' || a == 'Y') {
+
+            game();
+
+        }
+        else {
+            cout << "\t\t\t\t\t\033[1;34m           Good buy!\033[0m" << endl;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    game();
+    startGame();
+
     return 0;
 }
