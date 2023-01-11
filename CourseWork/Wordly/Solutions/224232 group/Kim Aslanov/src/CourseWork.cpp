b5827cf772a45ae105cb2c71376eef74d321b48c
unknown <iqezban@gmail.com>
2023-01-12 01:51:12 +0400

    12.01

--- CourseWork/Wordly/Solutions/224232 group/Kim Aslanov/src/CourseWork.cpp
+++ CourseWork/Wordly/Solutions/224232 group/Kim Aslanov/src/CourseWork.cpp
@@ -3,29 +3,39 @@
 
 #include <iostream>
 #include <fstream>
+#include <array>
 #include "lib/words.h"
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <stdio.h> 
+
 
 using namespace std;
 
 int enterNumber() {
     int n;
     cout << " \t\t\t\t\tВыберите колличество букв в угадываемом слове (от 4 до 7)" << endl;
-    cin >> n;
-    if (n < 4 || n > 7) {
-        cout << " \t\t\t\t\tWrong number, try again" << endl;
-            return enterNumber();
+    while (true) {
+        cin >> n;
+        if (n < 4 || n > 7) {
+            cout << " \t\t\t\t\tНеверное колличество букв" << endl;
+        }
+        else {
+            break;
+        }
     }
-    return n;
-}      
-
-
+       return n;
+    
+    }
 
 int randomWordIndex(int m) {
-    
-   int number = enterNumber();
+
+    int number = enterNumber();
     srand(time(0));
     int indexCount = countLinesInFile(openFileForWordsWithLenght(number)) - 1;
-    
+
     return rand() % indexCount;
 }
 
@@ -33,23 +43,16 @@ string randomWord(int n) {
     string* words = readWords(n);
     int i = randomWordIndex(n);
     string word = words[i];
-
+    cout << word << endl;
     return word;
 }
+
 
 string userWord() {
     cout << " \t\t\t\t\tВведите cлово" << endl;
     string w;
     cin >> w;
-    
-   /* int pos = s.read(readWords(7)); // поиск
-
-   
-    if (pos->find(w) == -1)
-    {
-        cout << "Нет слова в базе";
-    }*/
-        return w;
+    return w;
 }
 /*string comparision(string randomWord, string userWord) {
         for (int i = 0; i < randomWord.length() - 1; i++) {
@@ -64,42 +67,46 @@ string userWord() {
         }
         return 0;
 }*/
-
-
-
-//void check() {
-//   int n = enterNumber();
-//    readWords[n].find(UserWord());
-//
-//}
-// 
+void sravnit() {
+    
+    int num = enterNumber();
+    string randomW = randomWord(num);
+    int length = countLinesInFile(openFileForWordsWithLenght(num)) - 1;
+    for (int j = 0; j <= length; j++) {
+        randomW[j] = randomW[j].substr(0, 4);
+    }
+}
 int startGame() {
     // Start Game
-
-    string randomW = randomWord(enterNumber());
-       
+    int num = enterNumber();
+    string randomW = randomWord(num);
     
-    for (int i = 5; i >= 0; i--)
+    int ok=0;
+    for (int chance = 5; chance >= 0; chance--)
     {
-        cout << "У вас " << i << " попыток(а)" << endl;
+        
         string userW = userWord();
-
-
-
-        //comparision(random, UserWord());
-
+        cout << "У вас " << chance << " попыток(а)" << endl;
+      
         for (int i = 0; i < randomW.length() - 1; i++)
         {
+            
             if (randomW[i] == userW[i]) {
                 cout << " " << userW[i] << " ";
-
+                ok++;
             }
             else {
                 cout << " * ";
             }
-
         }
         cout << endl;
+
+        if (ok == randomW.length() - 1) {
+            cout << "\t\t\t\t\t\t   Супер! Вы выиграли!" << endl;
+            break;
+        }
+
+
         for (int i = 0; i < randomW.length() - 1; i++) {
             for (int j = 0; j < userW.length() - 1; j++) {
                 if (i != j && randomW[i] == userW[j]) {
@@ -108,28 +115,37 @@ int startGame() {
 
             }
         }
-
     }
-    cout << "Спасибо за игру" << endl;
-    return -1;
-}void vonaPlay() {
-    int vona;
-    if (startGame() = -1) {
-        cout << " Желаете сыграть еще раз? Y or N" << endl;
+            return 0;
+}
+    
+void vonaPlay() {
+    char vona;
+    while (true) {
+        cout << "\t\t\t\t\tЖелаете сыграть еще раз? Y or N" << endl;
         cin >> vona;
         if (vona == 'y' || vona == 'Y') {
+            cout << "\t\t\t     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - " << endl;
+            cout << "\t\t\t\t\t\t   Супер! Давайте сыграем еще раз =)" << endl;
+            cout << endl;
             startGame();
         }
         if (vona == 'n' || vona == 'N') {
-            return;
-        }
+            cout << "\t\t\t\t\tЖаль, до скорых встреч!" << endl;
+            break;
+        }   
         else
             vonaPlay();
     }
+
+      
 }
-    int main() {
-        startGame();
-        vonaPlay();
+
+int main() {
+    cout << "\t\t\t     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - " << endl;
+    cout << "\t\t\t\t\t\t   Добро пожаловать в игру ВИСЕЛИЦА" << endl;
+    startGame();
+    vonaPlay();
     return 0;
 }
 
