d0c4679406d5975e2050194b75a7ba1b3698ceb3
itacjik <vova.makogonenko@gmail.com>
2023-01-13 01:11:04 +0300

    volodya

--- CourseWork/Wordly/Solutions/224232 group/Vladimir Makogonenko/src/CourseWork.cpp
+++ CourseWork/Wordly/Solutions/224232 group/Vladimir Makogonenko/src/CourseWork.cpp
@@ -1,176 +1,170 @@
 ﻿// CourseWork.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+
 #include <fstream>
 #include "lib/words.h"
-
-
-
 #include <iostream>     // Ввод/Вывода
 #include <vector>       // Динамический массив
 #include <string>       // Строка
 #include <algorithm>    // Алгоритмы 
 #include <cctype>       // 
-#include <Windows.h>    //
-// Подключил библиотеки
+using namespace std;
 
+const int START_RANGE = 4;   // Мин значение  
+const int END_RANGE = 7;
+const int WORD_LENGTH = 5;
+const int NOT_MATCHED = 0;
+const int PARITAL_MATCHED = 1;
+const int MATCHED = 2;
+const int NUMBER_OF_GUESSES = 5;
 
-const int WORD_LENGTH = 5;      // Длина слова
-const int NOT_MATCH = 0;        // Кол-во совпадений
-const int PARITAL_MATCH = 1;    // Частичное совпадение
-const int MATCH = 2;            // Совпадение
-
-
-void toUpperCase(std::string &input)    // Функция верхнего регистра
+void toUpperCase(string &input)
 {
-    std::transform(input.begin(), input.end(), input.begin(), [](unsigned char c) { return std::toupper; });
+    transform(input.begin(), input.end(), input.begin(), [](unsigned char c) { return toupper(c); });
 }
 
+int newGame(){  // Запуск/перезапуск
+    int wordLength;   
+    cout << "Введите длину слова (от " << START_RANGE << " до " <<  END_RANGE <<") " <<endl;
+    cin >> wordLength;
 
-void printWordle(std::vector<std::string> tries, std::vector<std::vector<int>> matches, int currentTry)
-    // Функция вывода "Wordle"
-{
-    system("cls");      // Чистка консоли
-
-    std::cout << "$$\      $$\                           $$\ $$\           " << std::endl;
-    std::cout << "$$ | $\  $$ |                          $$ |$$ |          " << std::endl;
-    std::cout << "$$ |$$$\ $$ | $$$$$$\   $$$$$$\   $$$$$$$ |$$ | $$$$$$\  " << std::endl;
-    std::cout << "$$ $$ $$\$$ |$$  __$$\ $$  __$$\ $$  __$$ |$$ |$$  __$$\ " << std::endl;
-    std::cout << "$$$$  _$$$$ |$$ /  $$ |$$ |  \__|$$ /  $$ |$$ |$$$$$$$$ |" << std::endl;
-    std::cout << "$$$  / \$$$ |$$ |  $$ |$$ |      $$ |  $$ |$$ |$$   ____|" << std::endl;
-    std::cout << "$$  /   \$$ |\$$$$$$  |$$ |      \$$$$$$$ |$$ |\$$$$$$$\ " << std::endl;
-    std::cout << "\__/     \__| \______/ \__|       \_______|\__| \_______|" << std::endl;
-
-    for (int i = 0; i <= currentTry && i < tries.size(); i++){
-        std::string separator = "-";
-        std::string padding = "  |";
-        std::string text = "  |";
-
-        for (int j = 0; j < tries[i].length(); j++){
-            separator += "------";                // Разделитель
-            padding += "     |";                  // Набивка
-            char value = toupper(tries[i][j]);
-            text += "  ";
-
-            if (matches[i][j] == PARITAL_MATCH){
-                text += "\033[33m";
-            } else if (matches[i][j] == MATCH){
-                text += "\033[032m";
-            }
-
-            text += value;
-
-            if (matches [i][j] == PARITAL_MATCH || matches[i][j] == MATCH){
-                text += "\033[0m";
-            }
-
-            text += "  |";
-        }
-
-        if (i == 0){
-            std::cout << separator << std::endl;
-        }
-
-        std::cout << padding << std::endl;
-        std::cout << text << std::endl;
-        std::cout << padding << std::endl;
-        std::cout << separator << std::endl;
+    if (wordLength < START_RANGE || wordLength > END_RANGE){
+        cout << " Ты дурак ? Введи от " << START_RANGE << " до " << END_RANGE << endl;
+        cin >> wordLength;
+        cout << endl; 
     }
+
+    return wordLength;
 }
 
-
-
-void markMatch(std::vector<std::vector<int>> &matches, int tryIndex, std::string target, std::string guess)
-    // Совпадения ввода со целевым словом
+string getRandomWord(int wordLength)
 {
-    for (int i = 0; i < guess.length(); i++){
-        matches[tryIndex][i] = NOT_MATCH;
+    string* rangeSecretWord = readWords(wordLength);
+    srand(unsigned(time(NULL)));
+    int length = countLinesInFile(openFileForWordsWithLenght(wordLength));
+    int numberOfWord = rand()%length;
+
+    string secretWord = rangeSecretWord[numberOfWord-1];
+    cout << secretWord << endl;
+    return secretWord;
+}
+
+bool isValidWord(string word)
+{
+    int wordLength = newGame();
+    return word.length() == wordLength && word.find_first_not_of("abcdefghijklmnopqrstuvwxyz") == string::npos;
+}
+
+void markMatches(vector<vector<int>> &matches, int currentTry, string targetWord, string guessWord)
+{
+    for (int i = 0; i < guessWord.length(); i++){
+        matches[currentTry][i] == NOT_MATCHED;
     }
 
-    for (int j = 0; j < guess.length(); j++){
-        for (int i = 0; i < target.length(); i++){
-            if (guess[j] == target[i]){
+    for (int i = 0; i < guessWord.length(); i++){
+        for (int j = 0; j < targetWord.length(); j++){
+            if(guessWord[i] == targetWord[j]){
                 if (i == j){
-                    matches[tryIndex][j] = MATCH;
+                    matches[currentTry][i] = MATCHED;
                     break;
                 } else {
-                    matches[tryIndex][j] = PARITAL_MATCH;
+                    matches[currentTry][i] = PARITAL_MATCHED;
                 }
             }
         }
     }
 }
 
-
-bool isValid(std::string word)
+bool isAllMatched(string targetWord, string guessWord)
 {
-    // Проверка слова из словоря
-    return word.length() == WORD_LENGTH && word.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ") == std::string::npos;
-}
-
-
-
-std::string getRandomWord()         // Функция для генерации слова
-{
-    // (Получить случайное слово из словоря) 
-    return "ready";
-}
-
-
-
-bool isAllMatch(std::string target, std::string guess)
-    // Проверка на совпадение слова
-{
-    for (int i = 0; i < guess.length(); i++){
-        if (guess[i] != target[i])
-        return false;
+    for (int i = 0; i < targetWord.length(); i++){
+        if (targetWord[i] = guessWord[i]) return false;
     }
+
     return true;
-} 
+}
 
-int main(int, char **)
+void print(vector<vector<int>> matches, vector<string> guesses, int currentGuessIndex)
 {
-    int numberOfTries = 6;      // Число попыток
-    std::vector<std::string> tries(numberOfTries);
-    std::vector<std::vector<int>> matches(numberOfTries, std::vector<int>(WORD_LENGTH));
-    std::string targetWord = getRandomWord();
+    for (int i = 0; i <= currentGuessIndex && i < guesses.size(); i++){
+        string separator = "-";
+        string padding = "|";
+        string text = "|";
 
-    toUpperCase(targetWord);    // Используем функию верхнего регистра
+        for (int j = 0; j < guesses[i].length(); j++){
+            separator += "------";
+            padding += "     |";
+            char value = toupper(guesses[i][j]);
+            text += "   ";
+            cout << matches[i][j] << endl;
+            if (matches[i][j] == PARITAL_MATCHED){
+                text += "\033[33m";
+            } else if (matches[i][j] == MATCHED) {
+                text += "\033[32m";
+            }
 
-    std::string input;
+            text += value;
+
+            if (matches[i][j] == PARITAL_MATCHED || matches[i][j] == MATCHED) {
+                text += "\033[0m";
+            }
+
+            text += " |";
+        }
+
+        if (i == 0) {
+            cout  << separator << endl;
+        }
+
+        cout << padding << endl;
+        cout << text << endl;
+        cout << padding << endl;
+        cout << separator << endl;
+    }
+}
+
+int main()
+{
+    int newGame();
+    int wordLength;
+    // int wordLength;
+    vector<string> guesses(NUMBER_OF_GUESSES);
+    vector<vector<int>> matches(NUMBER_OF_GUESSES, vector<int>(WORD_LENGTH));
+    string targetWord = getRandomWord(wordLength);
+    
+    string input;
+    
+    print(matches, guesses, -1);
+
     int currentTry = 0;
 
-    while (currentTry < numberOfTries)
+    while (currentTry < NUMBER_OF_GUESSES)
     {
         do
         {
-            std::cout << "Please enter your guess (word length must be " + std::to_string(WORD_LENGTH) + ") or typr Q to quit :";
-            std::getline(std::cin, input);
-            toUpperCase(input);
-        }
-        while (input != "Q" && !isValid(input));
+            cout << "Please enter your guess (word length must be " + to_string(wordLength) + ") or type 'q' to quit: ";
+            getline(cin, input);
 
-        if (input == "Q")
+        } while (input != "q" && !isValidWord(input));
+
+        if (input == "q")
         {
-            std::cout << "Quit game" << std::endl;
+            cout << "Quit game" << endl;
             break;
         }
         
-        tries[currentTry] = input;
-        currentTry++;
-    }
+        guesses[currentTry] = input;
 
-    markMatch(matches, currentTry, targetWord, input);
+        markMatches(matches, currentTry, targetWord, input);
 
-    printWordle(tries, matches, currentTry);
+        print(matches, guesses, currentTry);
 
-    if (isAllMatch(targetWord, input))
-    {
-        std::cout << "Found the word" << std::endl;
-        
-    } else if (currentTry == numberOfTries){
-        std::cout << "You didn`t find the word" << std::endl;
+        if (isAllMatched(targetWord, input)) {
+            cout << "!!!!!!!!" << endl;
+            break;
+        }
+
+
+        currentTry ++;
     }
     
-
-    return 0;
 }
