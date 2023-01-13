7773bec94534f567b398714e525eefb114a944cb
Ernest_Kotoiants <ernest@kotoyants.com>
2023-01-13 03:40:11 -0800

    3

--- CourseWork/Wordly/Solutions/224232 group/Ernest Kotoiants/src/CourseWork.cpp
+++ CourseWork/Wordly/Solutions/224232 group/Ernest Kotoiants/src/CourseWork.cpp
@@ -9,67 +9,82 @@
 #include <stdio.h> 
 using namespace std;
 
-
-
-void game()
-{
-    cout << "\t\t\t\t\t  \033[1;34m   Lets play the game 'HANGMAN'!\033[0m" << endl;
-    cout << " \t\t\t\t\t \033[1;34m Choose, how much char in word(4-7): \033[0m" << endl;
-    int nChar, i;
-    string randomWord;
-
-    while (true) {g
-
+int Countletter() {
+    int nChar;
+    while (true) {
         cin >> nChar;
-        if (nChar < 4  && nChar > 7) {
+        if (nChar < 4 || nChar > 7) {
             cout << "\t\t\t\t\t \033[1;31m    Fail, choose from 4 to 7\033[0m" << endl;
-
         }
         else {
+            return nChar;
             break;
         }
+
     }
-    cout << "\t\t\t\t\t     \033[1;30m    You have 10 attempts\033[0m " << endl;
-    
-        srand(time(0));
-        int length = countLinesInFile(openFileForWordsWithLenght(nChar));
-        i = rand() % length;
-        string* words = readWords(nChar);
-        //cout << words[i] << endl;
-        randomWord = words[i];
-    
+}
+
+string randomWordd(int nChar) {
+    int i;
+    string randomWord;
+
+    srand(time(0));
+    int length = countLinesInFile(openFileForWordsWithLenght(nChar));
+    i = rand() % length;
+    string* words = readWords(nChar);
+    //cout << words[i] << endl;
+    randomWord = words[i];
+
 
     for (int i = 0; i < nChar; i++)
     {
         cout << " _ ";
     }
     cout << endl;
-    string enterWord;
-    int rightChars = 0;
-    for (int chance = 9; chance >= 0; chance--) {
-        while (true) {
-            cin >> enterWord;
-            if (enterWord.length() > nChar) {
-                cout << " \033[1;31mWord is too long\033[0m" << endl;
-            }
+    return randomWord;
+}
 
-            if (enterWord.length() < nChar) {
-                cout << "\033[1;31mWord is too short\033[0m" << endl;
-            }
-            else {
+string enterWordd(string randomWord, int nChar) {
+    string enterWord;
+    while (true) {
+        cin >> enterWord;
+        if (enterWord.length() > nChar || enterWord.length() < nChar) {
+            cout << " \033[1;31mWord is too long or too short\033[0m" << endl;
+        }
+        else {
+            break;
+        }
+    }
+    return enterWord;
+}
+
+void letterInWord(string enterWord, string randomWord, int nChar) {
+    for (int i = 0; i < randomWord.length() - 1; i++) {
+        for (int j = 0; j < randomWord.length() - 1; j++)
+        {
+            if (i != j && randomWord[i] == enterWord[j]) {
+                cout << "\033[1;31m" << enterWord[j] << "\033[0m" << "\033[1;30m in the word on another place!\033[0m" << endl;
                 break;
             }
-        }
-        for (int i = 0; i < randomWord.length() - 1; i++) {
-            for (int j = 0; j < randomWord.length() - 1; j++)
-            {
-                if (i != j && randomWord[i] == enterWord[j]) {
-                    cout << "\033[1;31m" << enterWord[j] << "\033[0m" << "\033[1;30m in the word on another place!\033[0m" << endl;
-                    break;  //возможно удалить!
-                }
 
-            }
         }
+    }
+}
+
+
+void game()
+{
+    cout << "\t\t\t\t\t  \033[1;34m   Lets play the game 'HANGMAN'!\033[0m" << endl;
+    cout << " \t\t\t\t\t \033[1;34m Choose, how much char in word(4-7): \033[0m" << endl;
+    int nChar = Countletter();
+    string randomWord = randomWordd(nChar);
+    cout << "\t\t\t\t\t     \033[1;30m    You have 10 attempts\033[0m " << endl;
+
+    string enterWord;
+    for (int chance = 9; chance >= 0; chance--) {
+        int rightChars = 0;
+        enterWord = enterWordd(randomWord, nChar);
+        letterInWord(enterWord, randomWord, nChar);
         for (int i = 0; i < randomWord.length() - 1; i++)
         {
             if (randomWord[i] == enterWord[i]) {
@@ -84,9 +99,7 @@ void game()
             cout << "\t\t\t\t\t \033[1;32m   You won the Game! Congrats!\033[0m" << endl;
             break;
         }
-        else {
-            rightChars = 0;
-        }
+
 
         cout << endl << "\t\t\t\t\t    \033[1;30m        Attempts - \033[0m" << "\033[1;31m" << chance << "\033[0m" << endl;
         if (chance == 0) {
