9da5f8069f3931f120ad98e962cd2c7374abe8d4
GeoBearD <geobeardstrateg1@gmail.com>
2023-01-14 14:25:20 -0800

    gameversion2

--- CourseWork/Wordly/Solutions/224232 group/Dimitri Avetisyan/src/CourseWork.cpp
+++ CourseWork/Wordly/Solutions/224232 group/Dimitri Avetisyan/src/CourseWork.cpp
@@ -10,11 +10,11 @@
 using namespace std;
 
 
-// ошибка при вводе буквы вместо цифры
+
 // ошибка, если в слове две одинаковые буквы
 
 
-const int attempts = 10;
+const int attempts = 7;
 
 
 
@@ -24,7 +24,7 @@ bool ifWordIsValid(int length, string* words, string enterWord, int countOfChars
     length = countLinesInFile(openFileForWordsWithLenght(countOfChars));
     for (int i = 0; i < length; i++)
     {
-        if (enterWord == words[i].substr(0, words[i].size() - 1))
+        if (enterWord == words[i].substr(0, countOfChars))
         {
             validWord = true;
         }
@@ -35,12 +35,19 @@ bool ifWordIsValid(int length, string* words, string enterWord, int countOfChars
 void game(int attempts)
 {
     cout << "\t\t\t\t\t  \033[1;34m   Lets play the game 'HANGMAN'!\033[0m" << endl;
-    cout << " \t\t\t\t\t \033[1;34m Choose, how much char in word(4-7): \033[0m" << endl;
+    cout << "\t\t\t\t  \033[1;34m    Try to guess the word by entering other english words\033[0m" << endl;
+    cout << " \t\t\t\t\t \033[1;34m            Choose level\033[0m" << endl;
+    cout << " \t\t\t\t\t \033[1;34mEnter how much letters in word do u want:\033[0m" << endl;
+    cout << " \t\t\t\t\t \033[1;34m            4,5,6 or 7?\033[0m" << endl;
+
     int countOfChars;
     
     while (true) {
-
-        cin >> countOfChars;
+        while (!(cin >> countOfChars)) {        //пока не будет введено нормальное число, выполняем цикл 
+            cout << "\t\t\t\t\t  \033[1;31m    Enter NUMBER from 4 to 7\033[0m " << endl;        
+            cin.clear();                //сбрасываем коматозное состояние cin
+            fflush(stdin);              //очищаем поток ввода
+        }
         if (countOfChars < 4 || countOfChars > 7) {
             cout << "\t\t\t\t\t \033[1;31m    Fail, choose from 4 to 7\033[0m" << endl;
 
@@ -50,12 +57,16 @@ void game(int attempts)
         }
     }
     cout << "\t\t\t\t\t     \033[1;30m    You have " << "\033[1;31m" << attempts << "\033[0m" << "\033[1;30m attempts\033[0m " << endl;
-
+    cout << "\t\t\t\t\t     \033[1;30m           +---+\033[0m " << endl;
+    cout << "\t\t\t\t\t     \033[1;30m           |   |\033[0m " << endl;
+    cout << "\t\t\t\t\t     \033[1;30m           O   |\033[0m " << endl;
+    cout << "\t\t\t\t\t     \033[1;30m   o/          |\033[0m " << endl;
+    cout << "\t\t\t\t\t     \033[1;30m  /0           |\033[0m " << endl;
+    cout << "\t\t\t\t\t     \033[1;30m   Л         ====\033[0m " << endl;
     srand(time(0));
     int length = countLinesInFile(openFileForWordsWithLenght(countOfChars));
     int i = rand() % length;
     string* words = readWords(countOfChars);
-    cout << words[i] << endl;
     string randomWord;
     randomWord = words[i];
 
@@ -68,7 +79,7 @@ void game(int attempts)
 
     string enterWord;
     int rightChars = 0;
-    for ( attempts = 9; attempts >= 0; attempts--) {
+    for ( attempts=attempts-1; attempts >= 0; attempts--) {
         while (true) {
             cin >> enterWord;
             if (ifWordIsValid(length, words, enterWord, countOfChars))
@@ -101,16 +112,29 @@ void game(int attempts)
         }
         if (rightChars == randomWord.length() - 1) {
             cout << "\t\t\t\t\t \033[1;32m     You won the Game! Congrats!\033[0m" << endl;
+            cout << "\t\t\t\t\t     \033[1;30m           +---+\033[0m " << endl;
+            cout << "\t\t\t\t\t     \033[1;30m           |   |\033[0m " << endl;
+            cout << "\t\t\t\t\t     \033[1;30m           O   |\033[0m " << endl;
+            cout << "\t\t\t\t\t     \033[1;30m   o/          |\033[0m " << endl;
+            cout << "\t\t\t\t\t     \033[1;30m  /0           |\033[0m " << endl;
+            cout << "\t\t\t\t\t     \033[1;30m   Л         ====\033[0m " << endl;
             break;
         }
         else {
             rightChars = 0;
         }
 
-        cout << endl << "\t\t\t\t\t    \033[1;30m        Attempts - \033[0m" << "\033[1;31m" << attempts << "\033[0m" << endl;
+        cout << endl << "\t\t\t\t\t    \033[1;30m         Attempts - \033[0m" << "\033[1;31m" << attempts << "\033[0m" << endl;
         if (attempts == 0) {
-            cout << "\t\t\t\t\t     \033[1;31m      GAME OVER!\033[0m" << endl;
-            cout << "\t\t\t\t\t\t \033[1;31m Word was - \033[0m" << "\033[1;30m" << randomWord << "\033[0m" << endl;
+            cout << "\t\t\t\t\t     \033[1;30m           +---+\033[0m " << endl;
+            cout << "\t\t\t\t\t     \033[1;30m           |   |\033[0m " << endl;
+            cout << "\t\t\t\t\t     \033[1;30m          o|   |\033[0m " << endl;
+            cout << "\t\t\t\t\t     \033[1;30m         //0   |\033[0m " << endl;
+            cout << "\t\t\t\t\t     \033[1;30m          //   |\033[0m " << endl;
+            cout << "\t\t\t\t\t     \033[1;30m             ====\033[0m " << endl;
+            cout << "\t\t\t\t\t     \033[1;31m          GAME OVER!\033[0m" << endl;
+            cout << "\t\t\t\t\t     \033[1;31m     You were HANGED! Loser!\033[0m" << endl;
+            cout << "\t\t\t\t\t\t \033[1;31m   Word was - \033[0m" << "\033[1;30m" << randomWord << "\033[0m" << endl;
         }
     }
 }
@@ -118,8 +142,21 @@ void game(int attempts)
 void startGame() {
 
     char a;
-    while (true) {
+    while (true)
+    {
+        cout << endl;
+        cout << endl;
+        cout << "\033[1; 30m=========================================================================================================================================================\033[0m " << endl;
         cout << "\t\t\t\t\t  \033[1;34m      Do u want to play again?\033[0m" << endl;
+        cout << "\t\t\t\t\t     \033[1;30m           +---+\033[0m " << endl;
+        cout << "\t\t\t\t\t     \033[1;30m           |   |\033[0m " << endl;
+        cout << "\t\t\t\t\t     \033[1;30m           O   |\033[0m " << endl;
+        cout << "\t\t\t\t\t     \033[1;30m   o/          |\033[0m " << endl;
+        cout << "\t\t\t\t\t     \033[1;30m  /0           |\033[0m " << endl;
+        cout << "\t\t\t\t\t     \033[1;30m   Л         ====\033[0m " << endl;
+        cout << endl;
+        cout << endl;
+        
         cout << "\t\t\t\t\t  \033[1;34m        Enter Y/y if u want.\033[0m" << endl;
         cin >> a;
         if (a == 'y' || a == 'Y') {
