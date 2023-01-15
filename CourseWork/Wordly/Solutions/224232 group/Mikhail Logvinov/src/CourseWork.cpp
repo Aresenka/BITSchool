63bff48fcbb4330dcd60abdee63d5b934fef316b
lmbfirst1 <sonniymoderator@gmail.com>
2023-01-15 14:59:51 +0400

    15.01 poslednyaa redaktura

--- CourseWork/Wordly/Solutions/224232 group/Mikhail Logvinov/src/CourseWork.cpp
+++ CourseWork/Wordly/Solutions/224232 group/Mikhail Logvinov/src/CourseWork.cpp
@@ -6,6 +6,10 @@
 #include "lib/words.h"
 #include <ctime>
 #include <vector>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stdio.h> 
 
 using namespace std;
 const int firstNumb = 4;
@@ -18,7 +22,7 @@ int numberOne() //рекурсия, чтобы пользователь ввел
 	cout << "введите число от 4 до 7 " << endl;
 	cin >> number;
 	if (number < firstNumb || number > lastNumb) {
-		cout << "попробуй снова" << endl; //тут хочу написать все варианты неправильного ввода 
+		cout << "попробуй снова" << endl; 
 		return numberOne();
 	}
 	else {
@@ -27,40 +31,116 @@ int numberOne() //рекурсия, чтобы пользователь ввел
 		return number;
 	}
 }
-//второй метод хз пока  реализовать лучше в мейне или до, а в мейне просто сослаться
-// 
-//
-void game(int number) { // вынести эту функцию из мейна 
+
+bool charInWord() {
+
+}
+
+bool charInRightPosition() {
+
+}
+
+void game(int number) { // функция выбора рандомного слова из библиотеки
 	string* rangeHiddenWord = readWords(number);
 	srand(unsigned(time(NULL)));
 	int length = countLinesInFile(openFileForWordsWithLenght(number));
 	int numberOfWord = rand() % length;
 
-	string hiddenWord = rangeHiddenWord[numberOfWord - 1];
+	string hiddenWord = rangeHiddenWord[numberOfWord]; // убрал от сюда -1
+	cout << hiddenWord << endl;
 	string word;
-
-	for (int i = 0; i < attempts; i++)
+	string guessedWord;
+	for (int n = 0; n < number; n++)
 	{
-		cout << "Введите слово из " << number << "букв" << endl;
-		for (int i = 0; i < number; i++)
-		{
-			cout << "*";
-		}
+		guessedWord[n] = '*';
+	}
+
+	for (int i = 1; i <= attempts; i++)
+	{
+		cout << guessedWord << endl;
+		cout << "Введите слово из " << number << " букв " << endl;
+		
 		cin >> word;
-		for (i = 0; i < hiddenWord.length(); i++) {
-			if (hiddenWord[i] == word[i]) {//прописать функцию сравнивания длины слов 
-				cout << word[i];
+		int h;
+		for (h = 0; h < hiddenWord.length() -1; h++) {
+			if (charInWord()) { // проверка вхождения символа в загаданное слово
+				if (charInRightPosition()) { // проверка того, стоит ли символ на прпавильной позиции
+					cout << hiddenWord[h];
+					guessedWord[h] = hiddenWord[h];
+				}
+				else {
+					// Добавление символа в массив отгаданных на неправильной позиции
+					cout << "*";
+				}
 			}
-			else { cout << "*"; }
-			cout << endl;
+			else {
+				cout << "*";
+			}
 		}
-		if (hiddenWord == word)
+		// Вывод букв с неправильной позицией
+		cout << endl;
+		if (hiddenWord == guessedWord)
 		{
 			cout << "YOU ARE WINNER!";
 			break;
 		}
-	} // подскажите поджаста, почему не работает)))))00)
+		else {
+			/*if (word.length() > number || word.length() < number) {
+				cout << "неправильная длина слова" << endl;
+				cin >> word;
+			}*/
+			if (hiddenWord != word && i == attempts){
+				cout << "YOU ARE Проиграли " << endl;
+			}
+			
+		}
+	} 
 }
+void wrongNumberOfSymbols(string word, int number) {
+	if (word.length() > number || word.length() < number) {
+		game(number); //  могу ли я так вызывать функцию
+		cout << endl;
+		cin >> word;
+	}
+}
+
+//void dashesInWord(string hiddenWord, string word) {   //функция сравнения слов 
+//	
+//	vector <char> wrongWords;
+//	for (int i = 0; i < hiddenWord.length(); i++) {
+//		if (hiddenWord[i] == word[i]) {
+//			cout << word[i];
+//		}
+//		else {
+//			cout << "_";
+//			wrongWords.push_back(word[i]);
+//		}
+//	}
+//	cout << endl << "БУКВЫ НЕ НА СВОЕМ МЕСТЕ" << endl;
+//
+//	for (int i = 0; i <= wrongWords.size(); i++) {
+//		for (int y = 0; y <= hiddenWord.length(); y++) {
+//			if (wrongWords[i] == hiddenWord[y]) {
+//				cout << wrongWords[i];
+//			}
+//		}
+//	}
+//}
+
+
+//*string enterWord(string hiddenWord, int number) {   // другая функция сравнения длинны слова
+//	string enterWord;
+//	while (true) {
+//		cin >> enterWord;
+//		if (enterWord.length() > number || enterWord.length() < number) {
+//			cout << " неправельная длинна слова " << endl;
+//		}
+//		else {
+//			break;
+//		}
+//	}
+//	return enterWord;
+//}
 
 
 
