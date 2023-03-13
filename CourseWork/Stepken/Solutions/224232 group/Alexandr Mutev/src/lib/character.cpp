0792d9a5721fd1ce6abe0a7fd56a89d3b08dd60e
macbook <sanya8923@gmail.com>
2023-03-14 00:29:31 +0400

    Исправил ссылки на указатели в заголовках

--- CourseWork/Stepken/Solutions/224232 group/Alexandr Mutev/src/lib/character.cpp
+++ CourseWork/Stepken/Solutions/224232 group/Alexandr Mutev/src/lib/character.cpp
@@ -1,10 +1,13 @@
 #include <iostream>>
-#include <character.h>
+#include "character.h"
 
 using namespace std;
 
 Character::Character(){
     this->health = health;
+    // Head head = Head();
+    // Body body = Body();
+    // Legs legs = Legs();
 }
 Character::virtual ~Character(){
 
